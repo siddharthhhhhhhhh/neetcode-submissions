2ca01adbe33ddd415c0972ad25d1cb998e8a8c7b
siddharthhhhhhhhh <138208062+siddharthhhhhhhhh@users.noreply.github.com>
2026-08-04 12:56:57 +0530

    Update: daily-temperatures - submission-0

--- Data Structures & Algorithms/daily-temperatures/submission-0.py
+++ Data Structures & Algorithms/daily-temperatures/submission-0.py
@@ -1,17 +1,13 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        list1 = []
-        if len(temperatures) == 1:
-            list1.append(0)
-            return list1
-        for i in range(len(temperatures)-1):
-            count = 0
-            for j in range(i+1, len(temperatures)):
-                if temperatures[i] < temperatures[j]:
-                    count = j - i
-                    break
-            list1.append(count)
-        list1.append(0)
-        return list1
+        
+        res = [0] * len(temperatures)
+        stack = []  # pair: [temp, index]
 
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT, stackInd = stack.pop()
+                res[stackInd] = i - stackInd
+            stack.append((t, i))
+        return res
         
