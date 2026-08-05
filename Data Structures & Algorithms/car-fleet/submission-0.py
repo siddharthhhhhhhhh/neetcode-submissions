c4cd47b3fd261e00d0f4123fb46a7d55ef5a1a6d
siddharthhhhhhhhh <138208062+siddharthhhhhhhhh@users.noreply.github.com>
2026-08-05 12:13:10 +0530

    Update: car-fleet - submission-0

--- Data Structures & Algorithms/car-fleet/submission-0.py
+++ Data Structures & Algorithms/car-fleet/submission-0.py
@@ -1,14 +1,30 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        pair = []
-        for p, s in zip(position, speed):
-            pair.append((p, s))
-        pair.sort(reverse=True)
-        stack = []
+        dict1 = {}
+        for i in range(len(position)):
+            dict1[position[i]] = speed[i]
+        position.sort()
+        rem_dis = []
+        for i in range(len(position)):
+            x = target - position[i]
+            rem_dis.append(x)
+        rate = []
+        for i in range(len(rem_dis)):
+            y = rem_dis[i]/dict1[position[i]]
+            rate.append(y)
+        z = rate.pop()
+        maxval = z
+        count = 1
+        while rate:
+            p = rate.pop()
+            if p <= maxval:
+                continue
+            else:
+                maxval = p
+                count += 1
+        return count
+          
 
-        for p, s in pair:
-            stack.append((target-p)/s)
-            if len(stack) >= 2 and stack[-1] <= stack[-2]:
-                stack.pop()
-        return len(stack)
+
+        
         
